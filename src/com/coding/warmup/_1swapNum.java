3dd2b88268bbf88f9206c62fdf21613ea369d3f5
LAPTOP-HDA61V4N\Ankit Shivane <ankitshivane.as@gmail.com>
2026-07-04 20:59:27 +0530

    system design first REST architecture

--- src/com/coding/warmup/_1swapNum.java
+++ src/com/coding/warmup/_1swapNum.java
@@ -2,8 +2,8 @@ package com.coding.warmup;
 
 public class _1swapNum {
     public static void main(String[] args) {
-        int n1 = 20;
-        int n2 = 10;
+        int n1 = 10;
+        int n2 = 20;
 
         System.out.println("Before Swapping: " + n1 + "--" + n2);
 // approach 1
@@ -15,11 +15,19 @@ public class _1swapNum {
 
         // approach 2
 
-        int k1 = 20;
-        int k2 = 10;
-        k1 = k1 ^ k2;
-        k2 = k1 ^ k2;
-        k1 = k1 ^ k2;
+
+        int k1 = 10; //1010
+        int k2 = 20; //10100
+        k1 = k1 ^ k2; // 11110 which is 30, so k1 becomes 30
+        k2 = k1 ^ k2; // 11110 ^ 10100 = 1010 which is 10, so k2 becomes 10
+        k1 = k1 ^ k2; // 11110 ^ 1010 = 10100 which is 20, so k1 becomes 20
         System.out.println("After Swapping Approach 1: " + k1 + "--" + k2);
+        /*
+        OUTPUT:
+        Before Swapping: 10--20
+        After Swapping Approach 1: 20--10
+        After Swapping Approach 1: 20--10
+
+         */
     }
 }
---
+++ src/com/coding/warmup/_2Replace0With5.java
@@ -0,0 +1,45 @@
+package com.coding.warmup;
+
+public class _2Replace0With5 {
+    /**
+     * https://www.geeksforgeeks.org/problems/replace-all-0s-with-5/1?page=1&category=Arrays&difficulty=Basic&sortBy=submissions
+     */
+
+    public static void main(String[] args) {
+        int n = 1004;
+        System.out.println(replaceZeroWithFiveApproach1(n));
+        System.out.println(replaceZeroWithFiveApproach2(n));
+    }
+
+    // Brute force approach
+    private static int replaceZeroWithFiveApproach1(int n) {
+        String s = String.valueOf(n);
+        s = s.replaceAll("0", "5");
+        return Integer.parseInt(s);
+    }
+
+    /**
+     * Optimal approach with O(log n) time complexity and O(1) space complexity Mathematically, we can replace 0 with 5 by adding 5 to the number and then subtracting 5 from the number. For example, if the number is 1004, we can add 5 to it to get 1009, and then subtract 5 from it to get 1004. This way, we can replace all the 0s with 5s in O(log n) time complexity and O(1) space complexity.
+     */
+
+    private static int replaceZeroWithFiveApproach2(int n) {
+        boolean isNegative = n < 0;
+        n = Math.abs(n);
+        if (n == 0) {
+            return 5;
+        }
+        int result = 0;
+        int place = 1;
+        while (n > 0) {
+            int lastDigit = n % 10;
+            if (lastDigit == 0) {
+                lastDigit = 5;
+            }
+            result = result + lastDigit * place;
+            place *= 10;
+            n = n / 10;
+        }
+
+        return result;
+    }
+}
